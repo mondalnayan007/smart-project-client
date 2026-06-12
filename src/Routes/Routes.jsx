2ef7f919d60e44942db52c9341314dbd67fec409
Nayan Mondal <n007kingkong@gmail.com>
2026-06-12 23:25:36 +0600

    created multiple components and set up router

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../Layout/RootLayout";
+import Home from "../Components/Home/Home";
+import AllProducts from "../Components/AllProducts/AllProducts";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children:[
+        {
+            index:true,
+            Component:Home
+
+        },
+        {
+            path:'/allproducts',
+            Component:AllProducts
+        }
+    ]
+    
+  },
+]);
